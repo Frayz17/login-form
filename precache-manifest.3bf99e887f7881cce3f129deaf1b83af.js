cde7db9c6da8129491852452e7a9bbce1a987704
Andrey Dmitriev <andrydmit@gmail.com>
2020-05-13 17:21:20 +0300

    Updates

---
+++ precache-manifest.3bf99e887f7881cce3f129deaf1b83af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbb460c1c5228ada50f642ff7f42156",
+    "url": "/login-form/index.html"
+  },
+  {
+    "revision": "6125fd4d520541de5a13",
+    "url": "/login-form/static/js/2.297c4235.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/login-form/static/js/2.297c4235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f128d2278e993f912eea",
+    "url": "/login-form/static/js/main.d93321d3.chunk.js"
+  },
+  {
+    "revision": "26d467fedd265d5398c7",
+    "url": "/login-form/static/js/runtime-main.eaede959.js"
+  },
+  {
+    "revision": "20c11024aa6a4807f871dba6b81496bd",
+    "url": "/login-form/static/media/background.20c11024.jpg"
+  },
+  {
+    "revision": "419119ec95e3986e12f0576ca21b2d21",
+    "url": "/login-form/static/media/background@2x.419119ec.jpg"
+  }
+]);
